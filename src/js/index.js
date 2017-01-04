d569f877d3ffb4c826059db9b549665829455998
yuxiziyoutiankong <1746922332@qq.com>
2017-01-04 17:32:35 +0800

    project at first time to commit !

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-	console.log($);
-})
+	// console.log($('.banner_box').width());
+	// console.log($(document).find('.yourName').width());
+	// console.log($(document).find('.yourName').width());
+
+
+
+	// test
+	var arr = [1,2,3,4,23,53];
+
+	console.log(Math.max.apply(null,arr));
+
+});
